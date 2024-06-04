068f9efbdc832d87c157b1cd990986f3e2008e13
lucio-cornejo <luciocornejo11@gmail.com>
2024-06-04 11:58:04 -0500

    Document project tasks

--- stores/target/README.md
+++ stores/target/README.md
@@ -2,18 +2,57 @@
 
 ## Tasks
 
-1. `tasks/grocery_categories_and_subcategories_url_extraction.py`
-1. `tasks/grocery_products_url_extraction.py`
+### Task 1
+
+- **Status**: Completed
+
+- **Goal**: Extract the URLs of the websites (sometimes is a grocery subcategory,
+but other times it's just a grocery category) which display multiple products
+- **File**: `tasks/grocery_categories_and_subcategories_url_extraction.py`
+- **Command for task execution**: Inside `target` directory, activate the 
+Python virtual environment and then execute `python -m tasks.grocery_categories_and_subcategories_url_extraction` .
 
 
-## Useful Target links
+### Task 2
 
-- **Extract product's UPC**: 
-    - Example: <https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin=88333669&pricing_store_id=1>
-    - Only the `tcin` parameter's value seems to be necessary to change in order for the UPC extraction to work for multiple products.
-    - For the products' url, as in the file `data/products_urls.json`, the `tcin` parameter's value corresponds to, for example:
-        - Product url: **https://www.target.com/p/van-zyverden-12-34-pineapple-lily-with-stainless-metal-planter-soil-and-growers-pot-eucomis/-/A-91835796#lnk=sametab**
-        - Product's `tcin` value: "91835796"
+- **Status**: Completed code, but pending execution and optimization .
+
+- **Goal**: For each grocery product, extract its main-image url, 
+title, `tcin` value, `store-id`, and other parameters required for the next main 
+task. However, `upc` and label info  extraction does not seem possible at this stage,
+but for the next main task .
+
+- **File**: `tasks/grocery_products_basic_info_extraction.py`
+
+- **Command for task execution**: Inside `target` directory, activate the 
+Python virtual environment and then execute `python -m tasks.grocery_products_basic_info_extraction` .
+
+
+**Pending** to use Python's seleniumwire package to intercept the get request response
+where the products' basic info is present (no need to extract it from website).
+
+Example: <https://redsky.target.com/redsky_aggregations/v1/web/plp_search_v2?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&category=5xsz2&channel=WEB&count=24&default_purchasability_filter=true&include_dmc_dmr=true&include_sponsored=true&new_search=false&offset=24&page=%2Fc%2F5xsz2&platform=desktop&pricing_store_id=671&spellcheck=true&store_ids=671%2C1246%2C924&useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F125.0.0.0+Safari%2F537.36&visitor_id=018EECDE90C702018E5EA8C77C7A126D&zip=15000>
+
+
+### Task 3
+
+- **Status**: Pending code, due to Target temporary (half a day) IP ban.
+
+- **Goal**: For each grocery product, extract it's label info and UPC.
+
+- **File**: `tasks/grocery_products_info_download.js`
+
+- **Command for task execution**: Inside `target` directory, 
+execute `node ./tasks/grocery_products_last_info_download.js`
+
+
+- Useful Target links
+  - **Extract product's UPC**: 
+      - Example: <https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin=88333669&pricing_store_id=1>
+      - Only the `tcin` parameter's value seems to be necessary to change in order for the UPC extraction to work for multiple products.
+      - For the products' url, as in the file `data/products_urls.json`, the `tcin` parameter's value corresponds to, for example:
+          - Product url: **https://www.target.com/p/van-zyverden-12-34-pineapple-lily-with-stainless-metal-planter-soil-and-growers-pot-eucomis/-/A-91835796#lnk=sametab**
+          - Product's `tcin` value: "91835796"
 
 
 ## Python packages installed
--- stores/target/tasks/grocery_products_last_info_download.js
+++ stores/target/tasks/grocery_products_last_info_download.js
@@ -4,7 +4,11 @@ const HTMLParser = require('node-html-parser');
 
 const log4js = require("log4js");
 log4js.configure({
-  appenders: { target: { type: "file", filename: "logs/grocery_products_info_download.log" } },
+  appenders: { target: { 
+    type: "fileSync", 
+    filename: "logs/grocery_products_last_info_download.log",
+    layout: { type: 'pattern', pattern: '%d-%c:[%p]: %m' },
+  } },
   categories: { default: { appenders: ["target"], level: "info" } },
 });
 const logger = log4js.getLogger("target");
@@ -12,19 +16,23 @@ const logger = log4js.getLogger("target");
 
 const productCategoryTreeCssSelector = '[data-test="@web/Breadcrumbs/BreadcrumbNav"]'
 
-const labelInfoSectionCssSelector = '[data-test="@web/site-top-of-funnel/ProductDetailCollapsible-LabelInfo"]';
-
 const specificationsSectionCssSelector = '[data-test="@web/site-top-of-funnel/ProductDetailCollapsible-Specifications"]';
 const specificationsCssSelector = '[data-test="item-details-specifications"] > div';
+// const productsUrlForDataAsJSON = 'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin=PRODUCT_TCIN_VALUE&pricing_store_id=1';
+const productsUrlForDataAsJSON = 'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=a5ae7fb188e78581614e4909f407462d8392b977&tcin=PRODUCT_TCIN_VALUE&pricing_store_id=1';
+
+const labelInfoSectionCssSelector = '[data-test="@web/site-top-of-funnel/ProductDetailCollapsible-LabelInfo"]';
+
+
+const productsURLS = JSON.parse(fs.readFileSync('data/products_urls.json', 'utf8'));
+// productsURLS.splice(4)
 
 /*
-const productsURLS = JSON.parse(fs.readFileSync('data/products_urls.json', 'utf8'));
-productsURLS.splice(4)
-
-*/
 const productsURLS = [{
-  'url' : 'https://www.target.com/p/oscar-mayer-jalapeno-cheddar-hot-dogs-16oz/-/A-91001109#lnk=sametab'
+  'url' : 'https://www.target.com/p/oscar-mayer-jalapeno-cheddar-hot-dogs-16oz/-/A-91001109#lnk=sametab',
+  'url' : 'https://www.target.com/p/kidfresh-frozen-chicken-meatballs-16-45oz/-/A-85356048#lnk=sametab'
 }]
+*/
 
 
 async.mapLimit(productsURLS, 3, async function(productURLS) {
@@ -32,13 +40,13 @@ async.mapLimit(productsURLS, 3, async function(productURLS) {
     const htmlText = await response.text();
 
     const root = HTMLParser.parse(htmlText);
-    console.log(productURLS['url']);
+    logger.info(`Product url: ${productURLS['url']}`);
     const productNewlyExtractedInfo = {};
 
     // Extract product's category tree
     const categoryTree = root.querySelector(productCategoryTreeCssSelector);
     if (categoryTree == null) {
-      console.error("Failed to extract product's category tree")
+      logger.error("Failed to extract product's category tree")
       return null;
     }
 
@@ -50,57 +58,41 @@ async.mapLimit(productsURLS, 3, async function(productURLS) {
     // Extract product's specification 
     const specificationsSection = root.querySelector(specificationsSectionCssSelector);
     if (specificationsSection == null) {
-      console.error("Failed to extract product's specification section")
+      logger.error("Failed to extract product's specification section")
       return { 'url': productURLS['url'], ...productNewlyExtractedInfo };
     }
 
     const productSpecifications = specificationsSection.querySelector(specificationsCssSelector);
     if (!productSpecifications) {
-      console.error("Failed to extract product's specifications")
-      return { 'url': productURLS['url'], ...productNewlyExtractedInfo };
+      logger.error("Failed to extract product's specifications")
+      
+      // Try to fetch the product's UPC via some Target's API
+      try {
+        const productTcinValue = productURLS['url'].match(/(\d+)(?=#lnk=sametab)/g)[0];
+        const productJsonUrl = productsUrlForDataAsJSON.replace('PRODUCT_TCIN_VALUE', productTcinValue);
+        logger.info(productJsonUrl);
+  
+        const newResponse = await fetch(productJsonUrl);
+        const newResponseJSON = await newResponse.json();
+
+        logger.info(newResponseJSON);
+        productNewlyExtractedInfo['upc'] = newResponseJSON.data.product.item.primary_barcode;
+      } catch (error) {
+        logger.error("Failed to fetch product's UPC");
+        logger.error(error);
+        return { 'url': productURLS['url'], ...productNewlyExtractedInfo };
+      }
     } 
 
-    /*
-
-    // Expected categories inner text (as regex pattern): "(\s*)categoryName(\s*):(\s+)categoryValue(\s*)"
-    const specificationCategoriesAndValues = Array.from(
-      specificationsSection.querySelectorAll('[data-test="item-details-specifications"] div:has(> b)')
-    ).map(e => {
-      return e.innerText
-      .toLowerCase().trim()
-      .split(':').map(text => text.trim())
-    });
-    const lowercasedSpecificationCategories = specificationCategoriesAndValues.map(e => e[0]);
-
-    console.log(
-      lowercasedSpecificationCategories
-    )
-
-    if (lowercasedSpecificationCategories.includes('upc')) {
-      productNewlyExtractedInfo['upc'] = specificationCategoriesAndValues[
-        lowercasedSpecificationCategories.indexOf('upc')
-      ][1];
-    } else if (lowercasedSpecificationCategories.includes('utc')) {
-      productNewlyExtractedInfo['utc'] = specificationCategoriesAndValues[
-        lowercasedSpecificationCategories.indexOf('utc')
-      ][1];
-    } else {
-      return null;
-    }
-    
     // Extract product's label info
     const labelInfoSection = root.querySelector(labelInfoSectionCssSelector);
     if (labelInfoSection == null) return { 'url': productURLS['url'], ...productNewlyExtractedInfo };
 
     productNewlyExtractedInfo['label_info'] = labelInfoSection.innerHTML;
-
     return { 'url': productURLS['url'], ...productNewlyExtractedInfo }
-
-    */
 }, (err, results) => {
-    if (err) throw err;
+    if (err) { logger.error(err); throw err;  }
     
-    console.log(results);
     fs.writeFileSync(
       'data/products_info.json',
       JSON.stringify(results.filter(e => e !== null), null, 4), 
