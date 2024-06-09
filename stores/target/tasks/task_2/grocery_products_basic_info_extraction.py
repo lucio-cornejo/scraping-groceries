7264bd9c9393e59d779ef330f41ccf16a13d0abb
lucio-cornejo <luciocornejo11@gmail.com>
2024-06-09 10:25:20 -0500

    Completed subtask-2.2, with execution duration time aprox 30 minutes

--- stores/target/tasks/task_2/grocery_products_basic_info_extraction.py
+++ stores/target/tasks/task_2/grocery_products_basic_info_extraction.py
@@ -32,7 +32,7 @@ with open('data/get_request_urls_for_task_2.2.json', 'r', encoding = 'utf-8') as
   ].to_dict('records')
 
 
-def extract_category_products_basic_info(
+def extract_grocery_group_products_basic_info(
   session: requests.Session, index: int, GET_request_url_dict: dict
 ):
   modified_GET_request_url = change_relevant_string_queries_values(
@@ -42,22 +42,22 @@ def extract_category_products_basic_info(
   task_2_2_logger.info(f'First modified GET request url: {modified_GET_request_url}')
 
   rsp_json = extract_get_request_json_response(session, modified_GET_request_url)
-  if rsp_json is None: raise Exception("Failed extraction of category's products basic info")
+  if rsp_json is None: raise Exception("Failed extraction of grocery's group products basic info")
 
   number_of_products = extract_number_of_products(rsp_json)
-  if number_of_products is None: raise Exception("Failed extraction of category's products basic info")
+  if number_of_products is None: raise Exception("Failed extraction of grocery's group products basic info")
   
   task_2_2_logger.info(f'Number of products: {number_of_products}')
 
   merged_bread_crumbs = merge_bread_crumbs_labels(rsp_json)
-  if merged_bread_crumbs is None: raise Exception("Failed extraction of category's products basic info")
+  if merged_bread_crumbs is None: raise Exception("Failed extraction of grocery's group products basic info")
 
   products_dicts_list = attempt_extraction_of_nested_dict_value(rsp_json, 'data;search;products')
-  if products_dicts_list is None: raise Exception("Failed extraction of category's products basic info")
+  if products_dicts_list is None: raise Exception("Failed extraction of grocery's group products basic info")
 
-  category_products_basic_info = []
+  group_products_basic_info = []
   for product_dict in products_dicts_list:
-    category_products_basic_info.append(extract_products_basic_info(product_dict))
+    group_products_basic_info.append(extract_products_basic_info(product_dict))
 
   # Change "offset" url query string parameters to fetch all products' basic info
   if number_of_products > MAX_NUM_OF_PRODUCTS_TO_FETCH_VIA_GET_REQUEST:
@@ -73,14 +73,14 @@ def extract_category_products_basic_info(
       rsp_json = extract_get_request_json_response(session, modified_GET_request_url)
       if rsp_json is None: 
         task_2_2_logger.critical(f'Failed GET request JSON extraction at iteration {i}')
-        raise Exception("Failed extraction of category's products basic info")
+        raise Exception("Failed extraction of grocery's group products basic info")
 
       products_dicts_list = attempt_extraction_of_nested_dict_value(rsp_json, 'data;search;products')
       if products_dicts_list is None: 
-        raise Exception("Failed extraction of category's products basic info")
+        raise Exception("Failed extraction of grocery's group products basic info")
       else:
         for product_dict in products_dicts_list:
-          category_products_basic_info.append(extract_products_basic_info(product_dict))
+          group_products_basic_info.append(extract_products_basic_info(product_dict))
 
       if i == number_of_extra_get_requests:
         task_2_2_logger.info('Completed all extra GET requests')
@@ -88,7 +88,7 @@ def extract_category_products_basic_info(
   """
   Save non duplicate data as CSV file
   """
-  cat_prods_basic_info_data_frame = pd.DataFrame(category_products_basic_info)
+  cat_prods_basic_info_data_frame = pd.DataFrame(group_products_basic_info)
 
   # Remove duplicate products, via their Target's API 'buy_url' value
   cat_prods_basic_info_data_frame = cat_prods_basic_info_data_frame[
@@ -99,11 +99,11 @@ def extract_category_products_basic_info(
   
   where_all_products_extracted = cat_prods_basic_info_data_frame.shape[0] == number_of_products
   if where_all_products_extracted:
-    task_2_2_logger.critical("Successful extraction of ALL category products's basic info")
+    task_2_2_logger.critical("Successful extraction of ALL grocery group's products basic info")
   else:
     task_2_2_logger.critical(f"Failed to extract basic info of {number_of_products - cat_prods_basic_info_data_frame.shape[0]} products")
 
-  task_2_2_logger.critical(f"Found {cat_prods_basic_info_data_frame.isna().sum().sum()} missing values in category's products data frame")
+  task_2_2_logger.critical(f"Found {cat_prods_basic_info_data_frame.isna().sum().sum()} missing values in grocery group's products data frame")
 
   cat_prods_basic_info_data_frame.to_csv(f'data/csv_files_for_task_3/index_{index}.csv', index = False)
   task_2_2_logger.critical('Completed local storage of data frame')
@@ -129,7 +129,7 @@ if __name__ == '__main__':
         )
 
       try:
-        extract_category_products_basic_info(gateway_session, index, GET_request_url_dict)
+        extract_grocery_group_products_basic_info(gateway_session, index, GET_request_url_dict)
         was_IP_banned = False
         break
       except Exception as e:
