f754f1daef886620c318dec8362d0c99a654a88f
lucio-cornejo <luciocornejo11@gmail.com>
2024-06-08 19:28:38 -0500

    Completed subtask 1.1: Execution time aprox 5 minutes

--- stores/target/tasks/task_1/grocery_categories_and_subcategories_url_extraction.py
+++ stores/target/tasks/task_1/grocery_categories_and_subcategories_url_extraction.py
@@ -11,19 +11,11 @@ from src.subcategories_scraper import (
 )
 
 
-if __name__ == '__main__':
-  """
-  Save into JSON file url of every Grocery category or subcategory
-  """
+def extract_urls_of_categories_and_subcategories():
   # Access Groceries homepage
   driver.set_page_load_timeout(config['selenium']['page_load_seconds_timeout'])
   driver.get(config['groceries_homepage']['url'])
-
-  # Show all categories
-  try:
-    click_show_all_categories_button_if_present()
-  except Exception as e:
-    task_1_1_logger.exception(e)
+  click_show_all_categories_button_if_present()
 
   categories_name_and_url = extract_categories_name_and_url()
 
@@ -40,8 +32,8 @@ if __name__ == '__main__':
     # Click button to show all categories, if available in website
     try:
       click_show_all_categories_button_if_present()
-    except Exception as e:
-      task_1_1_logger.exception(e)
+    except:
+      task_1_1_logger.info('All subcategories are shown by default in website')
 
     category_dict['subcategories'] = extract_categories_name_and_url()
 
@@ -61,6 +53,13 @@ if __name__ == '__main__':
   url_dicts_list = flatten_list_of_categories_and_subcategories_url(
     categories_name_and_url
   )
-  save_list_as_JSON(url_dicts_list, file_path = 'data/urls_for_task_2.1.json')
+  save_list_as_JSON(url_dicts_list, file_path = 'data/urls_for_task_1.2.json')
 
-  task_1_1_logger.info('Completed task')
+
+if __name__ == '__main__':
+  try:
+    task_1_1_logger.info('Started subtask')
+    extract_urls_of_categories_and_subcategories()
+    task_1_1_logger.info('Completed subtask')
+  except Exception as e:
+    task_1_1_logger.exception(e)
