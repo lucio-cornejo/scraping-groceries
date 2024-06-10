ead36895733a9df27faae404c871fa71631761d7
lucio-cornejo <luciocornejo11@gmail.com>
2024-06-10 00:18:26 -0500

    Save successful tests of products info extraction via GET request

--- stores/target/tasks/task_3/products_label_info_extraction.js
+++ stores/target/tasks/task_3/products_label_info_extraction.js
@@ -1,11 +1,75 @@
 const fs = require("fs"); 
+const Math = require('mathjs');
 const async = require("async");
-const HTMLParser = require('node-html-parser');
 
 const productsObjectsArray = JSON.parse(fs.readFileSync('data/unique_products_urls.json', 'utf8'));
+productsObjectsArray.splice(2);
 
-const expectedStringInNonPaginatedProductsWebpageHTML = "@web/slingshot-components/bubcat";
-const expectedStringInPaginatedProductsWebpageHTML = "@web/site-top-of-funnel/ProductCardPlaceholder";
+async function fetchProductInfo(index) {
+  const tcinOfProduct = productsObjectsArray[index]['tcin'].toString();
+  console.log(tcinOfProduct);
+
+  const randomThreeDigitInteger = Math.random(100, 999);
+
+  const GETRequesURL = 'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?' + 
+    [
+      'key=9f36aeafbe60771e321a7cc95a78140772ab3e96',
+      `tcin=${tcinOfProduct}`,
+      // 'is_bot=false',  // this does not seem necessary, but maybe it's useful
+      // 'store_id=671', 
+      'pricing_store_id=3991', // Does not seem to require value change per product
+      // 'has_pricing_store_id=true', 
+      // 'has_financing_options=true', 
+      // 'include_obsolete=true', 
+      `visitor_id=018EECDE90C702018E5EA8C77C7A${randomThreeDigitInteger}D`, 
+      // 'has_size_context=true', 
+      // 'skip_personalized=true', 
+      // 'skip_variation_hierarchy=true', 
+      'channel=WEB', 
+      `page=%2Fp%2FA-${tcinOfProduct}`
+    ].reduce((a, b) => a + '&' + b);
+  
+  console.log(GETRequesURL);
+  const response = await fetch(GETRequesURL);
+  console.log(response.status)
+
+  if (response.status == 200) {
+    const productJSON = await response.json();
+
+    let productItem;
+    if (productJSON['data']['product'].hasOwnProperty('children')) {
+      if (productJSON['data']['product']['children'].some(productItem => productItem?.tcin === tcinOfProduct)) {
+        productItem = productJSON['data']['product']['children']
+          .filter(productItem => productItem?.tcin === tcinOfProduct)
+          [0]
+          ['item'];      
+      } else {
+        throw new Error('Failed extracttion of product children with matching tcin value')
+      }
+    } else {
+      productItem = productJSON['data']['product']['item'];
+    }
+
+    productsObjectsArray[index]['nutrition_facts'] = productItem?.enrichment?.nutrition_facts;
+    productsObjectsArray[index]['bullet_descriptions'] = productItem?.product_description?.bullet_descriptions;
+    productsObjectsArray[index]['upc'] = productItem?.primary_barcode;
+    productsObjectsArray[index]['dpci'] = productItem?.dpci;
+    productsObjectsArray[index]['origin'] = productItem?.handling?.import_designation_description;
+
+    fs.writeFileSync(
+      `data/test_${index}_products_info_extraction.json`,
+      // `data/product_${index}_info.json`,
+      JSON.stringify(productsObjectsArray, null, 4), 
+      'utf8'
+    );
+    // console.log(productJSON);
+  }
+}
+
+
+const index = 0;
+fetchProductInfo(index);
+
 
 /*
 async.mapLimit(productsObjectsArray, 5, async function(productObject) {
@@ -65,21 +129,6 @@ async.mapLimit(productsObjectsArray, 5, async function(productObject) {
 });
 */
 
-async function fetchProductInfo(productObject) {
-  const url = productObject['url'];
-  const response = await fetch(url);
-
-  if (response.status == 200) {
-    const htmlText = await response.text();
-    const root = HTMLParser.parse(htmlText);
-    const body = root.querySelector('body');
-
-    return body.innerHTML;
-
-  }
-
-  return ''
-}
 /*
   "tcin": 47868632,
   "original_tcin": 47868632,
@@ -90,15 +139,10 @@ async function fetchProductInfo(productObject) {
   "merged_bread_crumbs": "Target/Grocery/Frozen Foods/Frozen Single Serve Meals"
 */
 
-const index = 0;
-console.log(
-  fetchProductInfo(productsObjectsArray[index])
-)
 
+// 'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin=47868632&is_bot=false&store_id=671&pricing_store_id=671&has_pricing_store_id=true&has_financing_options=true&include_obsolete=true&visitor_id=018EECDE90C702018E5EA8C77C7A126D&has_size_context=true&skip_personalized=true&skip_variation_hierarchy=true&channel=WEB&page=%2Fp%2FA-47868632'.split('&')
 
-'https://redsky.target.com/redsky_aggregations/v1/web/pdp_client_v1?key=9f36aeafbe60771e321a7cc95a78140772ab3e96&tcin=47868632&is_bot=false&store_id=671&pricing_store_id=671&has_pricing_store_id=true&has_financing_options=true&include_obsolete=true&visitor_id=018EECDE90C702018E5EA8C77C7A126D&has_size_context=true&skip_personalized=true&skip_variation_hierarchy=true&channel=WEB&page=%2Fp%2FA-47868632'.split('&')
-
-
+/*
 # %%
 tcin = 47868632
 tcin = 13234505
@@ -151,3 +195,5 @@ tcin = 13234505
   'useragent=Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+HeadlessChrome%2F125.0.0.0+Safari%2F537.36', 
   'visitor_id=018FFE27DA950201BD7C09943D12B677', 
   'zip=15400'
+
+  */
